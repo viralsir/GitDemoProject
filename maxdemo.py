27894bc74bc3a8d06c5bc9c091913947873d3415
ViralSir <viralsir2018@gmail.com>
2024-08-06 17:34:28 +0530

    max no functionality has been added.

---
+++ maxdemo.py
@@ -0,0 +1,8 @@
+no1=int(input("Enter the first number:"))
+no2=int(input("Enter the second number:"))
+
+if no1>no2:
+    print("The first number is greater than the second number.")
+else :
+    print("The second number is greater than the first number.")
+

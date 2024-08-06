cd618715751d58639b966fa2ee59beeeb54a4e37
ViralSir <viralsir2018@gmail.com>
2024-08-06 17:36:37 +0530

    positive no validation has been added.

--- maxdemo.py
+++ maxdemo.py
@@ -1,8 +1,11 @@
 no1=int(input("Enter the first number:"))
 no2=int(input("Enter the second number:"))
 
-if no1>no2:
-    print("The first number is greater than the second number.")
-else :
-    print("The second number is greater than the first number.")
 
+if no1>0 and no2>0:
+    if no1>no2:
+        print("The first number is greater than the second number.")
+    else :
+        print("The second number is greater than the first number.")
+else :
+    print("Nagetive input are not allowed.")

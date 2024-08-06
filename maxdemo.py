9dd8d6eaccdf1ae4673b92f6d10614ebad14bd71
viralsir <53177289+viralsir@users.noreply.github.com>
2024-08-06 17:39:42 +0530

    Update maxdemo.py
    change maxdemo from two number to three number.

--- maxdemo.py
+++ maxdemo.py
@@ -1,11 +1,13 @@
 no1=int(input("Enter the first number:"))
 no2=int(input("Enter the second number:"))
+no3=int(input("Enter the Third Number:"))
 
-
-if no1>0 and no2>0:
-    if no1>no2:
+if no1>0 and no2>0 and no3>0:
+    if no1>no2 and no1>no3:
         print("The first number is greater than the second number.")
-    else :
+    elif no2>no1 and no2>no3 :
         print("The second number is greater than the first number.")
+    else :
+        print("The Third number is a maximum")
 else :
     print("Nagetive input are not allowed.")
